82a9d6fddb67303cf436f3c5ba7a2110424601b6
Mathieu Bigorne <mathieu.bigorne@gmail.com>
2013-10-04 21:26:21 +0200

    Handle goTo and userExited command

--- src/main/scala/com/example/Elevator.scala
+++ src/main/scala/com/example/Elevator.scala
@@ -2,21 +2,32 @@ package com.example
 
 import List.fill
 
-case class Elevator(calls: List[Boolean] = fill(7)(false), currentFloor: Int = 0, isOpen: Boolean = false, command: String = "NOTHING") {
+case class Elevator(calls: List[Boolean] = fill(7)(false), goTos: List[Int] = Nil, currentFloor: Int = 0, isOpen: Boolean = false, command: String = "NOTHING") {
 
   def call(floor: Int) = {
-    Elevator(calls.updated(floor, true), currentFloor, isOpen, command)
+    Elevator(calls.updated(floor, true), goTos, currentFloor, isOpen, command)
   }
 
   def userHasEntered() = {
-    Elevator(calls.updated(currentFloor, false), currentFloor, isOpen, command)
+    Elevator(calls.updated(currentFloor, false), goTos, currentFloor, isOpen, command)
+  }
+
+  def goTo(floor: Int) = {
+    Elevator(calls, goTos :+ floor, currentFloor, isOpen, command)
+  }
+
+  def userHasExited() = {
+    Elevator(calls, goTos, currentFloor, isOpen, command)
   }
 
   def nextCommand() = this match {
-    case Elevator(_, _, true, _) => Elevator(calls, currentFloor, false, "CLOSE")
-    case Elevator(_, _, _, _) if calls.zipWithIndex.exists(call => call._1 && call._2 == currentFloor) => Elevator(calls, currentFloor, true, "OPEN")
-    case Elevator(_, _, _, _) if calls.zipWithIndex.exists(call => call._1 && call._2 > currentFloor) => Elevator(calls, currentFloor + 1, isOpen, "UP")
-    case Elevator(_, _, _, _) if calls.exists(call => call) => Elevator(calls, currentFloor - 1, isOpen, "DOWN")
-    case _ => Elevator(calls, currentFloor, isOpen, "NOTHING")
+    case Elevator(_, _, _, true, _) => Elevator(calls, goTos, currentFloor, false, "CLOSE")
+    case Elevator(_, firstGoTo :: _, _, _, _) if firstGoTo > currentFloor => Elevator(calls, goTos, currentFloor + 1, isOpen, "UP")
+    case Elevator(_, firstGoTo :: _, _, _, _) if firstGoTo < currentFloor => Elevator(calls, goTos, currentFloor - 1, isOpen, "DOWN")
+    case Elevator(_, firstGoTo :: tail, _, _, _) if firstGoTo == currentFloor => Elevator(calls, tail, currentFloor, true, "OPEN")
+    case Elevator(_, _, _, _, _) if calls.zipWithIndex.exists(call => call._1 && call._2 == currentFloor) => Elevator(calls, goTos, currentFloor, true, "OPEN")
+    case Elevator(_, _, _, _, _) if calls.zipWithIndex.exists(call => call._1 && call._2 > currentFloor) => Elevator(calls, goTos, currentFloor + 1, isOpen, "UP")
+    case Elevator(_, _, _, _, _) if calls.exists(call => call) => Elevator(calls, goTos, currentFloor - 1, isOpen, "DOWN")
+    case _ => Elevator(calls, goTos, currentFloor, isOpen, "NOTHING")
   }
 }
--- src/test/scala/com/example/ElevatorSpec.scala
+++ src/test/scala/com/example/ElevatorSpec.scala
@@ -26,7 +26,7 @@ class ElevatorSpec extends Specification {
       Elevator(currentFloor = 1, isOpen = true).userHasEntered().nextCommand().command must be equalTo "CLOSE"
     }
 
-    "return UP when calling from the 1st floor and elevator is at 3rd floor and has already been called at 4th" in {
+    "return UP twice when calling from the 2nd floor and 3rd floor and elevator is at 1st floor" in {
       var elevator = Elevator(currentFloor = 1).call(2).call(3)
 
       elevator = elevator.nextCommand()
@@ -38,6 +38,155 @@ class ElevatorSpec extends Specification {
       elevator.command must be equalTo "CLOSE"
       elevator = elevator.nextCommand()
       elevator.command must be equalTo "UP"
+    }
+
+    "return DOWN twice when calling from the 3rd floor and 2nd floor and elevator is at 4rd floor" in {
+      var elevator = Elevator(currentFloor = 4).call(3).call(2)
+
+      elevator = elevator.nextCommand()
+      elevator.command must be equalTo "DOWN"
+      elevator = elevator.nextCommand()
+      elevator.command must be equalTo "OPEN"
+      elevator = elevator.userHasEntered()
+      elevator = elevator.nextCommand()
+      elevator.command must be equalTo "CLOSE"
+      elevator = elevator.nextCommand()
+      elevator.command must be equalTo "DOWN"
+    }
+
+    "return DOWN twice when calling from the 2nd floor and 3rd floor and elevator is at 4rd floor" in {
+      var elevator = Elevator(currentFloor = 4).call(3).call(2)
+
+      elevator = elevator.nextCommand()
+      elevator.command must be equalTo "DOWN"
+      elevator = elevator.nextCommand()
+      elevator.command must be equalTo "OPEN"
+      elevator = elevator.userHasEntered()
+      elevator = elevator.nextCommand()
+      elevator.command must be equalTo "CLOSE"
+      elevator = elevator.nextCommand()
+      elevator.command must be equalTo "DOWN"
+    }
+
+    "return UP and then OPEN when elevator is at 2nd floor, a user enter and ask to go to floor 3" in {
+      var elevator = Elevator(currentFloor = 2, isOpen = true)
+      elevator = elevator.userHasEntered().goTo(3).nextCommand()
+      elevator.command must be equalTo "CLOSE"
+      elevator = elevator.nextCommand()
+      elevator.command must be equalTo "UP"
+      elevator = elevator.nextCommand()
+      elevator.command must be equalTo "OPEN"
+    }
+
+    "return DOWN and then OPEN when elevator is at 2nd floor, a user enter and ask to go to floor 1" in {
+      var elevator = Elevator(currentFloor = 2, isOpen = true)
+      elevator = elevator.userHasEntered().goTo(1).nextCommand()
+      elevator.command must be equalTo "CLOSE"
+      elevator = elevator.nextCommand()
+      elevator.command must be equalTo "DOWN"
+      elevator = elevator.nextCommand()
+      elevator.command must be equalTo "OPEN"
+      elevator = elevator.userHasExited()
+      elevator = elevator.nextCommand()
+      elevator.command must be equalTo "CLOSE"
+    }
+
+    "return DOWN and then OPEN when elevator is at 2nd floor, a user enter and ask to go to floor 1" in {
+      var elevator = Elevator(currentFloor = 2, isOpen = true)
+      elevator = elevator.userHasEntered().goTo(1).nextCommand()
+      elevator.command must be equalTo "CLOSE"
+      elevator = elevator.nextCommand()
+      elevator.command must be equalTo "DOWN"
+      elevator = elevator.nextCommand()
+      elevator.command must be equalTo "OPEN"
+      elevator = elevator.userHasExited()
+      elevator = elevator.nextCommand()
+      elevator.command must be equalTo "CLOSE"
+    }
+
+    "take first user to the right floor and then the second user" in {
+      var elevator = Elevator()
+      elevator = elevator.call(2).call(3).nextCommand()
+      elevator.command must be equalTo "UP"
+      elevator = elevator.nextCommand()
+      elevator.command must be equalTo "UP"
+      elevator = elevator.nextCommand()
+      elevator.command must be equalTo "OPEN"
+      elevator = elevator.userHasEntered()
+      elevator = elevator.nextCommand()
+      elevator.command must be equalTo "CLOSE"
+      elevator = elevator.goTo(1)
+      elevator = elevator.nextCommand()
+      elevator.command must be equalTo "DOWN"
+      elevator = elevator.nextCommand()
+      elevator.command must be equalTo "OPEN"
+      elevator = elevator.userHasEntered()
+      elevator = elevator.nextCommand()
+      elevator.command must be equalTo "CLOSE"
+      elevator = elevator.nextCommand()
+      elevator.command must be equalTo "UP"
+      elevator = elevator.nextCommand()
+      elevator.command must be equalTo "UP"
+      elevator = elevator.nextCommand()
+      elevator.command must be equalTo "OPEN"
+      elevator = elevator.userHasEntered()
+      elevator = elevator.nextCommand()
+      elevator.command must be equalTo "CLOSE"
+    }
+
+    "handle two user at the same floor going to the same floor" in {
+      var elevator = Elevator()
+      elevator = elevator.call(1)
+      elevator = elevator.nextCommand()
+      elevator.command must be equalTo "UP"
+      elevator = elevator.nextCommand()
+      elevator.command must be equalTo "OPEN"
+      elevator = elevator.userHasEntered()
+      elevator = elevator.userHasEntered()
+      elevator = elevator.nextCommand()
+      elevator.command must be equalTo "CLOSE"
+      elevator = elevator.nextCommand()
+      elevator.command must be equalTo "NOTHING"
+      elevator = elevator.goTo(2)
+      elevator = elevator.nextCommand()
+      elevator.command must be equalTo "UP"
+      elevator = elevator.nextCommand()
+      elevator.command must be equalTo "OPEN"
+      elevator = elevator.userHasExited()
+      elevator = elevator.userHasExited()
+      elevator = elevator.nextCommand()
+      elevator.command must be equalTo "CLOSE"
+    }
+
+    "handle two user at the same floor going up to different floors" in {
+      var elevator = Elevator()
+      elevator = elevator.call(1)
+      elevator = elevator.nextCommand()
+      elevator.command must be equalTo "UP"
+      elevator = elevator.nextCommand()
+      elevator.command must be equalTo "OPEN"
+      elevator = elevator.userHasEntered()
+      elevator = elevator.userHasEntered()
+      elevator = elevator.nextCommand()
+      elevator.command must be equalTo "CLOSE"
+      elevator = elevator.nextCommand()
+      elevator.command must be equalTo "NOTHING"
+      elevator = elevator.goTo(2)
+      elevator = elevator.goTo(3)
+      elevator = elevator.nextCommand()
+      elevator.command must be equalTo "UP"
+      elevator = elevator.nextCommand()
+      elevator.command must be equalTo "OPEN"
+      elevator = elevator.userHasExited()
+      elevator = elevator.nextCommand()
+      elevator.command must be equalTo "CLOSE"
+      elevator = elevator.nextCommand()
+      elevator.command must be equalTo "UP"
+      elevator = elevator.nextCommand()
+      elevator.command must be equalTo "OPEN"
+      elevator = elevator.userHasExited()
+      elevator = elevator.nextCommand()
+      elevator.command must be equalTo "CLOSE"
     }
   }
 }
